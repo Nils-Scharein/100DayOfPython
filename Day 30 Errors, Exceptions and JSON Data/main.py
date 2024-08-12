6b1971a3ae6edf3ea336b87032ea650973d55904
Nils <nilsscharein@yahoo.de>
2024-08-12 19:27:54 +0200

    Update

--- Day 30 Errors, Exceptions and JSON Data/main.py
+++ Day 30 Errors, Exceptions and JSON Data/main.py
@@ -1,9 +1,20 @@
 import pandas
 nato = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+#defines
 dic = {row["letter"]:row["code"] for (index, row) in nato.iterrows()}
-user_input = input("Please insert a Word for the Nato Alphabet")
-result = [dic[letter] for letter in user_input.upper()]
 
-print(result)
 
+a = True
+def generate_phonetic():
+    try:
+        user_input = input("Please insert a Word for the Nato Alphabet\n")
+        result = [dic[letter] for letter in user_input.upper()]
+        a = False
+    except KeyError:
+        print("Pls insert only letters")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
