636ad04fdc4d21058ba39cccfef87d25b60d55f8
Nils <nilsscharein@yahoo.de>
2024-08-14 21:32:03 +0200

    Complete Day 31
    Add CSV Day 31

--- Day 31 - Flash Card App/main.py
+++ Day 31 - Flash Card App/main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+import random
+import tkinter as tk
+from tkinter import ttk
+import os
+import pandas as pd
+import pandas.errors
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+current_card = {}
 
+# Defines
+BACKGROUND_COLOR = "#B1DDC6"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# Tkinter window setup
+window = tk.Tk()
+window.title("Flash Card Game")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# Paths to images
+card_back_image = tk.PhotoImage(file="images/card_back.png")
+card_front_image = tk.PhotoImage(file="images/card_front.png")
+right_image = tk.PhotoImage(file="images/right.png")
+wrong_image = tk.PhotoImage(file="images/wrong.png")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Canvas setup
+canvas = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas_card_image = canvas.create_image(400, 263, image=card_front_image)  # Centered image
+canvas_language_text = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))  # Centered text
+canvas_word_text = canvas.create_text(400, 263, text="Title", font=("Ariel", 40, "italic"))  # Centered text
+canvas.grid(row=0, column=0, columnspan=2)  # Make canvas span across two columns
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def unknown():
+    global current_card
+    nextCard()
+
+def known():
+    global current_card
+    word_list.remove(current_card)
+    wordsToLearn = pd.DataFrame.from_dict(word_list)
+    wordsToLearn.to_csv("data/wordsToLearn.csv", index=False)
+    nextCard()
+
+def nextCard():
+    global current_card
+    current_card = random.choice(word_list)
+    canvas.itemconfig(canvas_card_image, image=card_front_image)
+    canvas.itemconfig(canvas_language_text, fill="black")
+    canvas.itemconfig(canvas_language_text, text="French")
+    canvas.itemconfig(canvas_word_text, fill="black")
+    canvas.itemconfig(canvas_word_text, text=current_card["French"])
+    window.after(3000, func=flipCard)
+
+def flipCard():
+    canvas.itemconfig(canvas_card_image, image=card_back_image)
+    canvas.itemconfig(canvas_language_text, fill="white")
+    canvas.itemconfig(canvas_language_text, text="English")
+    canvas.itemconfig(canvas_word_text, fill="white")
+    canvas.itemconfig(canvas_word_text, text=current_card["English"])
+
+# Wrong button
+wrong_button = tk.Button(image=wrong_image, highlightthickness=0, borderwidth=0, command=unknown)
+wrong_button.grid(row=1, column=0)  # Stick to the right of the left column
+
+# Right button
+right_button = tk.Button(image=right_image, highlightthickness=0, borderwidth=0, command=known)
+right_button.grid(row=1, column=1)  # Stick to the left of the right column
+
+# Configure grid columns for centering
+window.grid_columnconfigure(0, weight=1)
+window.grid_columnconfigure(1, weight=1)
+
+try:
+    # Read the CSV file without including the index
+    data = pd.read_csv("data/wordsToLearn.csv")
+except (FileNotFoundError, pd.errors.EmptyDataError):
+    data = pd.read_csv("data/french_words.csv")
+    # Save the file without the index
+    data.to_csv("data/wordsToLearn.csv", index=False)
+
+# Convert the DataFrame to a list of dictionaries
+word_list = data.to_dict(orient="records")
+nextCard()
+
+window.mainloop()
