6a387ea9bef8cdbcf38fe1187cc2276af70c2b95
Nils <nilsscharein@yahoo.de>
2024-08-18 21:26:13 +0200

    Update Day 35 + 36

--- Day35 - Rain Alert/main.py
+++ Day35 - Rain Alert/main.py
@@ -1,19 +1,49 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
-
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+import csv
+import json
+import pandas as pd
+import requests
 
 
-quiz = QuizBrain(question_bank)
+Apikey_df = pd.read_csv("Api.txt")
+api_key = Apikey_df.loc[0, 'Key']
 
-while quiz.still_has_questions():
-    quiz.next_question()
+coordinates = pd.read_csv("coordinates.txt")
+coordinates.columns = coordinates.columns.str.strip()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+lat = coordinates.loc[0, 'lat']
+long = coordinates.loc[0, 'long']
+
+weather_params = {
+    "lat": 52.520008,
+    "lon": 13.404954,
+    "appid": api_key,
+    "cnt": 4
+}
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
+# Making the GET request to the OpenWeatherMap API
+response = requests.get(OWM_Endpoint, params=weather_params)
+
+# Open the file in write mode and dump the JSON data into it
+with open("data.json", "w") as data_file:
+    json.dump(response.json(), data_file, indent=4)
+
+# Optionally print the JSON response
+print(response.json())
+
+# Parse the JSON data into a dictionary
+weather_data = response.json()
+
+will_rain = False
+
+# Now you can safely iterate over the list in the response
+def check_weather_12_hours():
+    for hour_data in weather_data["list"]:
+        if hour_data["weather"][0]["id"] < 700:
+            return True
+        else:
+            return False
+
+if check_weather_12_hours() == True:
+    print("bring an umbrella")
